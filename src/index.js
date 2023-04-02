018358ef263095fcb79591c4482f21132b0268ee
olga-liushnia <olgaliusnia@gmail.com>
2023-04-03 00:55:32 +0200

    js transfer

---
+++ src/index.js
@@ -0,0 +1,90 @@
+function showCurrentTime() {
+    let currentTime = new Date();
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thurthday",
+      "Friday",
+      "Saturday"
+    ];
+  
+    let day = days[currentTime.getDay()];
+    let hour = currentTime.getHours();
+    if (hour < 10) {
+      hour = `0${hour}`;
+    }
+    let minute = currentTime.getMinutes();
+    if (minute < 10) {
+      minute = `0${minute}`;
+    }
+    let formatedTime = `${day} ${hour}:${minute}`;
+    return formatedTime;
+  }
+  
+  let currentTime = document.querySelector(".current-time");
+  currentTime.innerHTML = showCurrentTime(new Date());
+  setCurrentData();
+  
+  //part2
+  function getCity(event) {
+    event.preventDefault();
+    let inputCity = document.querySelector("#input-city");
+  
+    let city = inputCity.value;
+    let apiKey = "62231151ce343c4d68652e1617efc22f";
+    let unit = "metric";
+    let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiLink).then(showTemp);
+  }
+  function showTemp(responce) {
+    let currentCity = document.querySelector("#current-city");
+    currentCity.innerHTML = responce.data.name;
+    let temperature = Math.round(responce.data.main.temp);
+    let currentTemp = document.querySelector("#current-temp");
+    currentTemp.innerHTML = temperature;
+  }
+  
+  function getGeoCoords(position) {
+    let latitude = position.coords.latitude;
+  
+    let longitude = position.coords.longitude;
+  
+    let apiLink = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=62231151ce343c4d68652e1617efc22f`;
+    axios.get(apiLink).then(showTemp);
+  }
+  
+  function setCurrentData() {
+    navigator.geolocation.getCurrentPosition(getGeoCoords);
+  }
+  
+  let city = document.querySelector("#search-city");
+  city.addEventListener("submit", getCity);
+  
+  let currentCity = document.querySelector("#btn-current-city");
+  currentCity.addEventListener("click", setCurrentData);
+  
+  /*part3
+  
+  function toFarenheit() {
+    let f = 3 * 1.8 + 32;
+    let fRounded = Math.round(f);
+    let currentTemp = document.querySelector("#current-temp");
+    currentTemp.innerHTML = fRounded;
+  }
+  
+  function toCelsius() {
+    let currentTemp = document.querySelector("#current-temp");
+    currentTemp.innerHTML = 3;
+  }
+  
+  let farenheit = document.querySelector("#farenheit");
+  farenheit.addEventListener("click", toFarenheit);
+  
+  let celsius = document.querySelector("#celsius");
+  celsius.addEventListener("click", toCelsius);
+  */
+  
+  //week5hw
